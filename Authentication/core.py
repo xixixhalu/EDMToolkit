a59cc952dabc92637a77c89bd43ba1e09897e61c
Nitin Jamadagni <nitin.jamadagni@gmail.com>
2017-11-20 14:42:39 -0800

    complete authentication service

--- Authentication/core.py
+++ Authentication/core.py
@@ -12,6 +12,8 @@ Imports section
 from flask import Flask, jsonify, request
 import sys
 from pymongo import MongoClient
+import uuid
+import datetime as dt
 
 
 '''
@@ -34,6 +36,14 @@ def setupandexit():
 	db.create_collection("authentication")
 	db["authentication"].create_index([('username',pymongo.ASCENDING)],unique=True)
 
+def create_random_key():
+	return str(uuid.uuid4())
+
+def create_expiry_object():
+	return dt.datetime.now() + dt.timedelta(days = 30)
+
+
+
 
 
 '''
@@ -43,32 +53,94 @@ Main Functionalities
 def default():
 	response = {"status" : "success",\
 				"response" : "Use the following  model for service\n \
-				1. /register/<username> [response of a token] \n\
-				2. /authenticate/<username> [response of a authentication accept] \n\
-				3. /refreshtoken/<username> [renew token and send back new]\n\
+				1. /register/<username> [] [response of a token] \n\
+				2. /authenticate/<username> [send 'key' : key_value in header] [response of a authentication accept] \n\
+				3. /refreshtoken/<username> [send 'key' : key_value in header] [renew token and send back new]\n\
 				"}
 	return jsonify(response)
 
 @app.route('/authenticate/<string:username>')
 def authenticate(username):
 	response = {}
+
+	if not hasattr(request.args , 'key'):
+		response["status"] = "fail"
+		response["response"] = "'key' not sent in header"
+		return jsonify(response)
+
+
 	userprofile = db["authentication"].find_one(filter={"username" : username}, projection={'_id':False,'key':True, 'username' : True , 'validtill' : True})
-	if username.count() == 0:
+	if userprofile.count() == 0:
 		response["status"] = "fail"
 		response["response"] = "User not registered yet"
 		return jsonify(response)
 
-	# NITIN : NOTE : Check for expiry of token later
+	
 
 	if request.args.key == str(userprofile["key"]):
+		delta = dt.datetime.now() - userprofile["validtill"]
+		if not delta >= dt.timedelta():
+			response["status"] = "fail"
+			response["response"] = "try refreshing your token, it is expired"
+			return jsonify(response)
 		response["status"] = "success"
 		response["response"] = {"username" : userprofile["username"] , "Authenticated" : True}
 		return jsonify(response)
 	else:
-		response["status"] = "success"
+		response["status"] = "fail"
 		response["response"] = {"username" : userprofile["username"] , "Authenticated" : False}
 		return jsonify(response)
-	 
+
+
+@app.route('/register/<string:username>')
+def registerUser(username):
+	response = {}
+	userprofile = db["authentication"].find_one(filter={"username" : username} , projection = {'_id' : True , 'key' : False , 'username' : False , 'validtill' : False})
+	if userprofile.count() is not 0:
+		response["status"] = "fail"
+		response["response"] = "User already registered"
+		return jsonify(response)
+
+	key = create_random_key()
+	validtill = create_expiry_object()
+	userprofile = { "username" : username , "key" : key , "validtill" : validtill}
+	db["authentication"].insert_one(userprofile)
+	response["status"] = "success"
+	response["response"] = {"message" : "Successfully registerd user, check the object in 'returnvalue' to get the key" , "returnvalue" : userprofile}
+	return jsonify(response)
+
+
+@app.route('/refreshtoken/<string:username>')
+def refreshtoken(username):
+	response = {}
+	
+	if not hasattr(request.args , 'key'):
+		response["status"] = "fail"
+		response["response"] = "'key' not sent in header"
+		return jsonify(response)
+
+	userprofile = db["authentication"].find_one(filter={"username" : username}, projection={'_id':False,'key':True, 'username' : True , 'validtill' : True})
+	if userprofile.count() == 0:
+		response["status"] = "fail"
+		response["response"] = "User not registered yet"
+		return jsonify(response)
+
+	if request.args.key == str(userprofile["key"]):
+		key = create_random_key()
+		validtill = create_expiry_object()
+		db["authentication"].update_one(filter = {"username"  : username} , update = { '$set' : {'key' : key , 'validtill' : validtill} }, upsert = False)
+		userprofile = { "username" : username , "key" : key , "validtill" : validtill}
+		response["status"] = "success"
+		response["response"] = {"message" : "Successfully updated key, check the object in 'returnvalue' to get the key" , "returnvalue" : userprofile}
+	else:
+		response["status"] = "fail"
+		response["response"] = "wrong old key, provide correct old key to refresh token"
+
+	
+
+
+
+
 
 
 
